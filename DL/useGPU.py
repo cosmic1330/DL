5d6ba8e1fde4b57a263d21230533fecf9a7f85c4
楊竣宇\yang <ch20026103@yahoo.com.tw>
2022-04-03 10:49:49 +0800

    add mnist

---
+++ DL/useGPU.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.python.client import load_model
+
+physical_devices = tf.config.list_physical_devices('GPU')
+print("Num GPUs:", len(physical_devices))
+
+print(device_lib.list_local_devices())
+
+
+# 使用CPU
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]='-1' 
