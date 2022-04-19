5e08fd1aae1acc304d4fd7fabea81f7327a00329
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2022-04-19 15:00:11 +0800

    add

--- Component/CNN/mnist.py
+++ Component/CNN/mnist.py
@@ -42,9 +42,119 @@ print("X_test shape: ",X_test.shape)
 from keras.utils.np_utils import to_categorical 
 
 # convert to one-hot-encoding(one hot vectors)
-Y_train = to_categorical(train_labels, num_classes = 10)
+Y_train = to_categorical(train_labels)
 # convert to one-hot-encoding(one hot vectors)
-Y_test = to_categorical(test_labels, num_classes = 10)
+Y_test = to_categorical(test_labels)
 
 print(Y_train.shape)
 print(Y_test.shape)
+
+
+from sklearn.model_selection import train_test_split
+x_train, x_val, y_train, y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state = 2)
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Activation, MaxPool2D, Flatten, Dropout, BatchNormalization
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import EarlyStopping
+
+
+model = Sequential()
+
+#1. LAYER
+model.add(Conv2D(filters = 32, kernel_size = (3,3), padding = 'Same', input_shape=(28, 28, 1)))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+
+#2. LAYER
+model.add(Conv2D(filters = 32, kernel_size = (3,3), padding = 'Same'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+#3. LAYER
+model.add(Conv2D(filters = 64, kernel_size = (3,3), padding = 'Same'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+
+#4. LAYER
+model.add(Conv2D(filters = 64, kernel_size = (3,3), padding = 'Same'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+#FULLY CONNECTED LAYER
+model.add(Flatten())
+model.add(Dense(256))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(0.25))
+
+#OUTPUT LAYER
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+
+optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(optimizer = optimizer, loss = "categorical_crossentropy", metrics=["accuracy"])
+epochs = 5 # for better result increase the epochs
+batch_size = 127
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # 以每一張feature map為單位將平均值設為0
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # 以每一張feature map為單位將數值除以其標準差(上述兩步驟就是我們常見的Standardization)
+        samplewise_std_normalization=False,  #  將输入的每個樣本除以其自身的標準差。
+        zca_whitening=False,  # dimesion reduction
+        rotation_range=0.1,  # 隨機旋轉圖片
+        zoom_range = 0.1, #  隨機縮放範圍
+        width_shift_range=0.1,  #  水平平移，相對總寬度的比例
+        height_shift_range=0.1,  # 垂直平移，相對總高度的比例
+        horizontal_flip=False,  # 一半影象水平翻轉
+        vertical_flip=False)  # 一半影象垂直翻轉
+
+datagen.fit(x_train)
+
+my_callbacks = [
+    # validation loss 三個執行週期沒改善就停止訓練
+    EarlyStopping(patience=3, monitor = 'val_accuracy'),
+    # save the best weights
+    ModelCheckpoint(filepath="Component/CNN/mnist_model_weight.h5", verbose=1, save_best_only=True)
+]
+
+# # 載入最近的檢查點的權重
+# model.load_weights(checkpoint_filepath)
+# # 訓練 5 次
+# model.fit(x_train_norm, y_train, epochs=5, validation_split=0.2, callbacks=model_checkpoint_callback)
+
+# 降低資料佔據
+history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),shuffle=True,epochs=epochs, 
+                              validation_data = (x_val, y_val),
+                              verbose = 2, #verbose=2過程全顯示
+                              steps_per_epoch=x_train.shape[0] // batch_size,
+                              callbacks=my_callbacks) #we save the best weights with checkpointer
+
+
+# 繪圖
+plt.figure(figsize=(14,5))
+plt.subplot(1, 2, 1)
+plt.suptitle('Train Results', fontsize=10)
+plt.xlabel("Number of Epochs")
+plt.ylabel('Loss', fontsize=16)
+plt.plot(history.history['loss'], color='b', label='Training Loss')
+plt.plot(history.history['val_loss'], color='r', label='Validation Loss')
+plt.legend(loc='upper right')
+
+plt.subplot(1, 2, 2)
+plt.ylabel('Accuracy', fontsize=16)
+plt.plot(history.history['accuracy'], color='green', label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], color='orange', label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.show()
